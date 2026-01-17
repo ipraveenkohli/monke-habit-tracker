c9140023d3c9b370bd5be2de5ee8bb516642efdc
ipraveenkohli <praveenkohlilearner@gmail.com>
2026-01-17 20:14:35 +0530

    Create mainactivity.kt

---
+++ mainactivity.kt
@@ -0,0 +1,29 @@
+package com.habittrack
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.habittrack.ui.navigation.NavGraph
+import com.habittrack.ui.theme.HabitTrackTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HabitTrackTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
+                }
+            }
+        }
+    }
+}
